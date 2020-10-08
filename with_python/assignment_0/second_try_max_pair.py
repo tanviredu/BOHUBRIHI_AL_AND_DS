29b9ace3b2367b864d3e19394c05c3506f94a8d7
tanviredu <ornobtanvir.git@gmail.com>
2020-10-08 20:27:25 +0600

    added

--- with_python/assignment_0/second_try_max_pair.py
+++ with_python/assignment_0/second_try_max_pair.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def max_pair_wise_product(input_list):
+    input_list = set(input_list)
     max_1 = max(input_list)
     input_list.remove(max_1)
     max_2 = max(input_list)
@@ -8,9 +9,9 @@ def max_pair_wise_product(input_list):
     return m_product
 
 def test():
-    correct_result = [72,4000, 400000, 40000000, 4000000000, 400000000000, 40000000000000]
-    result = []
-    array_list = [[1,2,3,4,5,6,7,8,9],[10,30,40,100],[100,300,400,1000],[1000,3000,4000,10000],[10000,30000,40000,100000],[100000,300000,400000,1000000],[1000000,3000000,4000000,10000000]]
+    correct_result = [20]
+    array_list = [[5,5,4]]
+    result =[]
     for ar in array_list:
         result.append(max_pair_wise_product(ar))
     for itrue,itest in zip(correct_result,result):
